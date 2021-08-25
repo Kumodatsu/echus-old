918069c70985ad9564ee3ae64d7cfd98c94284bf
Kumodatsu <dragonkumo@gmail.com>
2021-08-25 22:54:16 +0200

    Add infinity constant

--- projects/echus-lib/include/echus/Math.hpp
+++ projects/echus-lib/include/echus/Math.hpp
@@ -1,11 +1,13 @@
 #pragma once
 #include <cmath>
+#include <limits>
 
 namespace echus::math {
 
     inline constexpr float
         PI          = 3.141592653589f,
-        C_12_ROOT_2 = 1.059463094359295264;
+        C_12_ROOT_2 = 1.059463094359f,
+        Infinity    = std::numeric_limits<float>::infinity();
 
     inline constexpr float angular(float f) {
         return 2.0f * PI * f;
